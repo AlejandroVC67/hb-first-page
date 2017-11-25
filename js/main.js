4b125c548cc28cbce90b721b6e6b7d7730bf71f5
AlejandroVC67 <vcalejandro97@gmail.com>
2017-11-25 18:32:09 -0500

    Initial desktop layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var btns;
 var images;
 var food;
+var flipper;
 window.onload = init;
 function init(){
     initVariables();
@@ -10,6 +11,8 @@ function init(){
 function initVariables(){
     images = document.getElementsByClassName('image-container-flipper');
     btns = document.getElementsByTagName('div');
+    flipper = document.getElementsByClassName('image-container-flipper');
+    //console.log(flipper);
 
 }
 
@@ -20,17 +23,28 @@ function initFlip(){
             const topic = btns[i].dataset.type;
             console.log(topic + 'este es el dataset');
             //removeClass(topic);
+            //rotation(flipper);
             flip(topic);
        });
     }
 }
 
+function rotation(flipper){
+    for(let q in flipper){
+        flipper[q].style.transform = "rotateY(180deg)";
+    }
+}
+
+
+
 function flip(topic){
+    
     console.log('removing classes');
     for(let j in btns){
         if(btns[j].dataset.type != topic){
             btns[j].classList.remove('front');            
             btns[j].classList.add('back');
+            btns[j].style.position = 'fixed';
         }
         else{
             btns[j].classList.add('front');
